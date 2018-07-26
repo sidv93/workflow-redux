201581bf635584cbfe0ca1b8eab51a0d0a23bfda
Siddharth Venkatesh <siddharth@asteria.co.in>
2018-07-26 17:54:28 +0530

    constants file

---
+++ src/app/app.constants.ts
@@ -0,0 +1,9 @@
+export class Configuration {
+    public authenticateUrl = 'http://localhost:3000/api/v1/user/login/';
+    public getBoardsUrl = 'http://localhost:3000/api/v1/boards/asteria';
+    public getListsUrl = 'http://localhost:3000/api/v1/lists/';
+    public getCardsUrl = 'http://localhost:3000/api/v1/cards/';
+    public createBoardUrl = 'http://localhost:3000/api/v1/board';
+    public createListUrl = 'http://localhost:3000/api/v1/list/';
+    public createCardUrl = 'http://localhost:3000/api/v1/card';
+}
--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
-  styleUrls: ['./cards.component.css']
+  styleUrls: ['./cards.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CardsComponent implements OnInit {
 
